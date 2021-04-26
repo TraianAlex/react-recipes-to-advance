d8f8a159713eaf4c17b6b99f6e4351d83a906239
Traian Alexandru <victor_traian@yahoo.com>
2021-04-26 14:25:38 -0400

    clean

--- react-recipes-master/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import './styles/index.css';
-
-const MOUNT_ELEMENT = document.getElementById('root');
-
-ReactDOM.render(<App />, MOUNT_ELEMENT);
